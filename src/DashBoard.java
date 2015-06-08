0a55d8f3eb54f5ee63b53c93a60bb13b223137f8
yogagb <yoga.gb@live.com>
2015-06-08 16:00:53 +0700

    Create DashBoard.java

---
+++ src/DashBoard.java
@@ -0,0 +1,17 @@
+
+public class DashBoard {
+	public int getSpeed(Engine a)
+	{
+		return a.speed;
+	}
+	public double getFuel(FuelTank a)
+	{
+		return a.fuelLevel;
+	}
+	public int getTemperature(Engine a)
+	{
+		return a.temperature;
+	}
+	//public odometer
+	
+}
